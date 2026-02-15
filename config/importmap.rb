fa00c66e939e7581503fa19cd79b6890c29c8dbd
Brian Douglas <brian@douglas.li>
2026-02-15 18:08:40 +0100

    Map 'controllers' to index.js in importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,7 +5,8 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 
-# Pin controllers explicitly
+# Pin controllers index and explicit controllers
+pin "controllers", to: "controllers/index.js"
 pin "controllers/application", to: "controllers/application.js"
 pin "controllers/index", to: "controllers/index.js"
 pin "controllers/trip_gear_controller", to: "controllers/trip_gear_controller.js"
