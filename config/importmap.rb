0472aac0176362e931ca494922f61c94b2897af4
Brian Douglas <brian@douglas.li>
2026-02-15 18:07:20 +0100

    Enable asset compilation and fix controller paths for production

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,8 @@ pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
+
+# Pin controllers explicitly
+pin "controllers/application", to: "controllers/application.js"
+pin "controllers/index", to: "controllers/index.js"
+pin "controllers/trip_gear_controller", to: "controllers/trip_gear_controller.js"
